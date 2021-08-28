9bf26b5a2e52138930436ac8fc7b7d7c0e9c8326
Benjongyanger <83170746+Benjongyanger@users.noreply.github.com>
2021-08-28 18:08:17 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 28 17:44:07 2021
+
+@author: Ben
+"""
+
+from math import pi
+
+r = float(input ("Enter radius of circle : "))
+
+print ("Area of the circle is: " + str(pi * r**2))
